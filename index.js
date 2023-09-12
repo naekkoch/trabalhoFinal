134044b6041308326e699dc648e112ae44752f7e
naekkoch <micaelaas3105@gmail.com>
2023-09-12 19:23:52 -0300

    o erick é chato para um cacete plmds

---
+++ index.js
@@ -0,0 +1,6 @@
+const nome = prompt("Informe seu nome")
+const peso = prompt("Informe seu pesoem kg")
+const idade = prompt("Informe sua idade")
+const tipoSanguineo = prompt("Informe seu tipo sanguineo")
+const dataDoação = prompt("Informe a data da sua ultima doação")
+
