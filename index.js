ccfb2aa0b331524ada5e6b9875052d54b7887d31
Erick <ericklopesalmeida8@gmail.com>
2023-09-12 21:56:54 -0300

    EU CODIGUEI MUITO

--- index.js
+++ index.js
@@ -1,6 +1,63 @@
-const nome = prompt("Informe seu nome")
-const peso = prompt("Informe seu pesoem kg")
-const idade = prompt("Informe sua idade")
-const tipoSanguineo = prompt("Informe seu tipo sanguineo")
-const dataDoação = prompt("Informe a data da sua ultima doação")
+const arrayDoadores = []
 
+function menu() {
+  const opcao = Number(
+    prompt(`
+    ===== SISTEMA DE CADASTRO DE DOADORES DE SANGUE =====
+    1. Cadastrar doador
+    2. Listar doadores
+    3. Buscar doador por tipo sanguíneo
+    4. Buscar doador por data da última doação
+    5. Sair
+    Escolha uma opção:`)
+  );
+
+  switch (opcao) {
+    case 1:
+      cadastrarDoador();
+      break;
+    case 2:
+      listarDoadores();
+      break;
+    case 3:
+      buscarTipoSangue();
+      break;
+    case 4:
+      buscarPorData();
+      break;
+    case 5:
+      break;
+    default:
+      alert("Opção inválida. Tente novamente.");
+      menu();
+  }
+}
+
+//
+const cadastrarDoador = () => {
+    const nome = prompt("Nome").toUpperCase()
+    const idade = Number(prompt("Idade"))
+    
+    if (isNaN(idade)) {
+      const idade = Number(prompt("COLOQUE UM NÚMERO SEU INFELIZ"))
+    }
+    
+    const peso = Number(prompt("Peso"))
+    const tipoSanguineo = prompt("Tipo Sanguíneo").toUpperCase()
+    const ultimaDoacao = Number(prompt("Última Doação"))
+
+    const doador = {
+        nome: nome,
+        idade: idade,
+        peso: peso,
+        tipoSanguineo: tipoSanguineo,
+        ultimaDoacao: ultimaDoacao,
+    }
+
+    console.log(doador)
+    arrayDoadores.push(doador)
+    menu()
+}
+//
+
+menu()
